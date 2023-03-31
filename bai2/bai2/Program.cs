dcb922daf93e74783f1d3fea1c45dfad5f54e4e9
Karratth <128267388+Karratth@users.noreply.github.com>
2023-03-31 12:53:13 +0700

    Add files via upload

---
+++ bai2/bai2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BAI_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap so nguyen");
+            int a = int.Parse(Console.ReadLine());
+            int i = 1;
+            string chuoi = "";
+            while (i <= 10)
+            {
+                chuoi += $@"{a} x {i} = {a * i} ; ";
+                i += 1;
+            }
+            
+            
+
+            Console.WriteLine("result: {0}",chuoi);
+            Console.ReadKey();
+        }
+    }
+}
